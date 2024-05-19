2949cb1c765b6a7fa9b7e16e785e2e10dfb98fbe
Scarrange <xMizor@gmail.com>
2024-05-19 19:37:53 +0200

    Datepicker fixed OMG endlich

--- app/components/customdatePicker.tsx
+++ app/components/customdatePicker.tsx
@@ -1,20 +1,25 @@
-// DatePicker.tsx
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+import { ClientOnly } from "remix-utils/client-only";
 
 const CustomDatePicker = () => {
   const [startDate, setStartDate] = useState<Date | null>(null);
-//TODO Fehler beheben
+  //TODO Fehler beheben
   return (
     <div className="form-control mt-3" style={{ maxWidth: "400px" }}>
-      <DatePicker
-        selected={startDate}
-        onChange={(date: Date | null) => setStartDate(date)}
-        className="form-control"
-        dateFormat="dd/MM/yyyy"
-        placeholderText="Datum"
-      />
+      <ClientOnly>
+        {() => (
+          <DatePicker
+            selected={startDate}
+            onChange={(date: Date | null) => setStartDate(date)}
+            className="form-control"
+            dateFormat="dd/MM/yyyy"
+            placeholderText="Datum"
+            showYearDropdown
+          />
+        )}
+      </ClientOnly>
     </div>
   );
 };
