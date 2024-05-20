5c60eeea35b540244ccc907e91d2455220f81b32
Robin Kraus <krro1024@h-ka.de>
2024-05-20 21:56:08 +0200

    navbar anpassung

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,29 +4,46 @@ import Login from "./login";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          <img
-            src="images/hochschule.png"
-            alt="Hochschule"
-            className="navbar-image"
-          />
-        </Link>
-        <div className="d-flex ms-5">
-          <Link to="/search" style={{ color: "white", textDecoration: "none" }}>
-            <button type="button" className="btn btn-primary btn-lg me-2">
-              Buch suchen
-            </button>
-          </Link>
-          <Link to="/new" style={{ color: "white", textDecoration: "none" }}>
-            <button type="button" className="btn btn-primary btn-lg">
-              Buch anlegen
-            </button>
+      <div className="container-fluid navbar-container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-brand">
+            <img
+              src="images/hochschule.png"
+              alt="Hochschule"
+              className="navbar-image"
+            />
           </Link>
         </div>
-        <div className="ms-auto">
-          <Login />
-          {/* TODO loggedIn ? Login : Logout */}
+
+        <div className="navbar-buttons">
+          <div className="d-flex ms-5">
+            <Link
+              to="/search"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              <button type="button" className="btn btn-primary btn-lg me-2">
+                Buch suchen
+              </button>
+            </Link>
+            <Link to="/new" style={{ color: "white", textDecoration: "none" }}>
+              <button type="button" className="btn btn-primary btn-lg">
+                Buch anlegen
+              </button>
+            </Link>
+          </div>
+          <div className="login-container">
+            <Login />
+            {/* TODO loggedIn ? Login : Logout */}
+          </div>
+        </div>
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-brand">
+            <img
+              src="images/hkaquer.png"
+              alt="Hochschule"
+              className="navbar-image"
+            />
+          </Link>
         </div>
       </div>
     </nav>
