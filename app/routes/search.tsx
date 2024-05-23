155266c088a2c88dc70d3263bf489030b1a28733
Robin Kraus <krro1024@h-ka.de>
2024-05-23 13:11:26 +0200

    suchformular

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,7 +1,23 @@
+import Input from "../components/input";
+import CheckBox from "../components/checkBox";
+import DropDown from "~/components/dropDown";
+
 export default function searchPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Bücher suchen</h1>
+      <div className="container d-flex flex-column align-items-center mt-5">
+        <h1>Suchformular</h1>
+        <Input placeholder="ISBN" />
+        <Input placeholder="Titel" />
+        <DropDown />
+        <div
+          className="container d-flex justify-content-around mt-3"
+          style={{ maxWidth: "400px" }}
+        >
+          <CheckBox text="JavaScript" />
+          <CheckBox text="TypeScript" />
+        </div>
+      </div>
     </div>
   );
 }
