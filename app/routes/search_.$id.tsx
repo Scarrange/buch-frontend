2f3cd8ea048cee1a2e431f57c313c11b6d38c21a
Scarrange <xMizor@gmail.com>
2024-06-19 07:15:38 +0200

    try catch auf Detailseite + Layout umstrukturiert, damit man bei Fehler kein update Button hat

--- app/routes/search_.$id.tsx
+++ app/routes/search_.$id.tsx
@@ -10,15 +10,20 @@ export const links: LinksFunction = () => {
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { id } = params;
+  let response;
 
-  const response = await fetch(`https://localhost:3000/rest/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    response = await fetch(`https://localhost:3000/rest/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (e) {
+    return json({ buch: null, id });
+  }
 
-  if (!response.ok) {
+  if (response.status !== 200) {
     return json({ buch: null, id });
   }
 
@@ -32,28 +37,32 @@ export default function BookDetailPage() {
 
   return (
     <div className="container">
-      <div className="container d-flex flex-column align-items-center mt-5 form-control div-bg mb-5">
-        <h1>Buchdetails</h1>
-        {buch ? <BuchItem {...buch} /> : <p>Buch nicht gefunden</p>}
-        <div className="container-fluid d-flex mt-3 mb-3">
-          <div>
-            <Link to={`/update/${id}`} state={buch}>
-              <button className="btn btn-primary btn-lg me-2 mt-2">
-                Ändern
-              </button>
-            </Link>
-            <Link to="/test">
-              <button className="btn btn-primary btn-lg mt-2">Löschen</button>
-            </Link>
+      {buch ? (
+        <div className="container d-flex flex-column align-items-center mt-5 form-control div-bg mb-5">
+          <h1>Buchdetails</h1>
+          <BuchItem {...buch} />
+          <div className="container-fluid d-flex mt-3 mb-3">
+            <div>
+              <Link to={`/update/${id}`} state={buch}>
+                <button className="btn btn-primary btn-lg me-2 mt-2">
+                  Ändern
+                </button>
+              </Link>
+              <Link to="/test">
+                <button className="btn btn-primary btn-lg mt-2">Löschen</button>
+              </Link>
+            </div>
+            <button
+              className="btn btn-secondary btn-lg ms-auto mt-2"
+              onClick={() => navigate(-1)}
+            >
+              Zurück
+            </button>
           </div>
-          <button
-            className="btn btn-secondary btn-lg ms-auto mt-2"
-            onClick={() => navigate(-1)}
-          >
-            Zurück
-          </button>
         </div>
-      </div>
+      ) : (
+        <p>Buch nicht gefunden</p>
+      )}
     </div>
   );
 }
