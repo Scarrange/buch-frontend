05fcc20aebd69cb3ef69da93af1eb16c42d037b0
Scarrange <xMizor@gmail.com>
2024-06-21 15:09:50 +0200

    dotenv entfernt, bootstrap.js import entfernt

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,9 +11,7 @@ import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-import dotenv from "dotenv";
 
-dotenv.config();
 const ABORT_DELAY = 5_000;
 
 export default function handleRequest(
