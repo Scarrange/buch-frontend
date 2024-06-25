ce44a6edf57d715d92e15b6f88f59dc4c69e15ff
Huema Yilmaz <102851390+HuemaYilmaz@users.noreply.github.com>
2024-06-25 13:05:57 +0200

    update
    update mit request erfolgreich

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -2,6 +2,7 @@ const Input = (props: {
   placeholder: string | undefined;
   name: string | undefined;
   required?: boolean;
+  defaultValue?: string;
 }) => {
   return (
     <input
@@ -11,6 +12,7 @@ const Input = (props: {
       style={{ maxWidth: "400px" }}
       name={props.name}
       required={props.required}
+      defaultValue={props.defaultValue}
     />
   );
 };
