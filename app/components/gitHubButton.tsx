577591803b4d5a3783929df1fd692245be066286
Scarrange <xMizor@gmail.com>
2024-06-26 04:04:55 +0200

    prettier

--- app/components/gitHubButton.tsx
+++ app/components/gitHubButton.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const GithubLoginButton: React.FC = () => {
   return (
     <div className="d-flex justify-content-center  mt-5 mb-3">
-      <button className="btn btn-outline-secondary d-flex align-items-center" type="button">
+      <button
+        className="btn btn-outline-secondary d-flex align-items-center"
+        type="button"
+      >
         <FontAwesomeIcon icon={faGithub} className="me-2" size="2x" />
         <span className="smaller-text">Sign up with Github</span>
       </button>
